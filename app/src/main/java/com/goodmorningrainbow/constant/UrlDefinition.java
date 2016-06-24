2c7c2711e0a4b074620eb4adb16100e2c1abf134
rrobbie1 <rrobbie1@naver.com>
2016-06-24 19:09:22 +0900

    update

--- app/src/main/java/com/goodmorningrainbow/constant/UrlDefinition.java
+++ app/src/main/java/com/goodmorningrainbow/constant/UrlDefinition.java
@@ -2,9 +2,9 @@ package com.goodmorningrainbow.constant;
 
 public class UrlDefinition {
 
-    public static String PhonePlus = "http://phonepluseapp2.com/url/index.php";
+    public static String PhonePlus = "http://test43.why-be.co.kr";
 
-    public static String FILE_UPLOAD = "http://test42.why-be.co.kr/file_upload.php";
+    public static String FILE_UPLOAD = "http://test43.why-be.co.kr/upload.php";
 
 
 }
